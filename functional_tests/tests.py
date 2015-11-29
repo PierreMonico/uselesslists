a6b8ce47babee9e4c5c7f908b21189ce2f012a8e
Pierre <p.monico@gmx.fr>
2015-11-29 22:22:41 +0300

    moved database outside main tree, updated FT to test staging site

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,23 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+        
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
     
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         #Frog visits the website to add some items he wants to remember 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         #He sees the website title and the header of the to do list 
         self.assertIn('To-Do', self.browser.title)
@@ -63,7 +78,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         
         #cat visits the site; there is no sign of frogs list 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         page_text = self.browser.find_element_by_tag_name('body').text
         
@@ -87,7 +102,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
             
     def test_layout_and_styling(self):
         #Frog goes to the home page 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         
         #he notices the inputbox is nicely centered 
